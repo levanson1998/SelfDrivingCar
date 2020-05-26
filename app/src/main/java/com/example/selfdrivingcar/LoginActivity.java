9e7a31bea7a3611a726c8c5d61b023b1bd2dcca8
levanson1998 <vanson1243@gmail.com>
2020-05-26 10:51:44 +0700

    login v1.2

--- app/src/main/java/com/example/selfdrivingcar/LoginActivity.java
+++ app/src/main/java/com/example/selfdrivingcar/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.selfdrivingcar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +13,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class LoginActivity extends AppCompatActivity {
     private EditText Name;
     private EditText Password;
     private TextView Info;
     private Button Login;
     private int Counter = 5;
+    private int timer =10;
+
+    private Handler customHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +57,25 @@ public class LoginActivity extends AppCompatActivity {
             Info.setText("Please Login To Use Application!!!" + String.valueOf(Counter));
             if (Counter == 0) {
                 Login.setEnabled(false);
+                new CountDownTimer(11000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        Info.setText("Please wait "+Integer.toString(timer));
+                        timer--;
+                    }
+
+                    public void onFinish() {
+                        timer=10;
+                        Counter=6;
+                        Login.setEnabled(true);
+                    }
+                }.start();
+
             }
         }
 
     }
+
+
+
 }
+
